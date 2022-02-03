944c0ba9e6d5811632ff55872b70a61e5bd49f66
4msha <firdausia1345@gmail.com>
2022-02-03 18:19:53 +0530

    restructured rademe and added geolocation

--- src/test/java/com/lambdatest/suite/SuiteTest02.java
+++ src/test/java/com/lambdatest/suite/SuiteTest02.java
@@ -1,4 +1,5 @@
 package com.lambdatest.suite;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
@@ -11,18 +12,18 @@ import org.testng.annotations.Test;
 import com.lambdatest.LambdaTestSetup;
 
 public class SuiteTest02 extends LambdaTestSetup {
-  
-   @Test
+
+    @Test
     public void test() throws Exception {
 
-	   open("http://www.google.co.uk");
+        open("http://www.google.com");
 
         $(By.name("q")).setValue("LambdaTest Automation").pressEnter();
 
         sleep(2000);
 
         Assert.assertEquals(title(), "LambdaTest Automation - Google Search");
-        status="passed";
+        status = "passed";
     }
-   
+
 }
--- src/test/java/com/lambdatest/suite/SuiteTest03.java
+++ src/test/java/com/lambdatest/suite/SuiteTest03.java
@@ -1,4 +1,5 @@
 package com.lambdatest.suite;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
@@ -15,14 +16,14 @@ public class SuiteTest03 extends LambdaTestSetup {
     @Test
     public void test() throws Exception {
 
-    	open("http://www.google.co.uk");
+        open("http://www.google.com");
 
         $(By.name("q")).setValue("LambdaTest Live Testing").pressEnter();
 
         sleep(2000);
 
         Assert.assertEquals(title(), "LambdaTest Live Testing - Google Search");
-        status="passed";
-       }
-   
+        status = "passed";
+    }
+
 }
